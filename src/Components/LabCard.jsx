f8901d9d5f67f9d2b076e49a26f884afe9ddb7fe
Min3th <minethweerasinghe@gmail.com>
2024-10-01 04:24:59 +0530

    Connected several endpoints for admin

--- src/Components/LabCard.jsx
+++ src/Components/LabCard.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
-const LabCard = ({ imgsrc, altname, labData, imgWidth = "200px", imgHeight = "200px" }) => {
+const LabCard = ({ imgsrc, altname, labData, imgWidth = "200px", imgHeight = "200px", onLabDelete }) => {
+  const [error, setError] = useState(null);
+
+  const deleteLab = async () => {
+    setError(null);
+    try {
+      const response = await axios.delete(
+        `http://ims-api-fbf3hheffacqe5ak.westus2-01.azurewebsites.net/api/admin/labs/${labData.labId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        },
+      );
+
+      console.log("deleted lab:", response.data);
+
+      if (onLabDelete) {
+        onLabDelete(labData.labId);
+      }
+    } catch (errror) {
+      console.error("Error when fetching res", error);
+      setError("Failed to load reservations");
+    }
+  };
+
   return (
-    <div className="w-[297px] h-[278px] bg-[#3C4D71] rounded-[60px] flex flex-col justify-center items-center relative cursor-pointer hover:scale-[1.1] transition duration-200 shadow-lg">
+    <div className="w-[297px] h-[278px] bg-[#3C4D71] rounded-[60px] flex flex-col justify-center items-center relative cursor-pointer hover:scale-[1.1] transition duration-200 shadow-lg gap-3 p-4">
       <img className="object-contain" src={imgsrc} alt={altname} style={{ width: imgWidth, height: imgHeight }} />
-      <p className="font-josefin-sans font-normal text-[20px] text-white leading-[20px] tracking-[0.06em] pb-4">
+      <p className="font-josefin-sans font-normal text-[20px] text-white leading-[20px] tracking-[0.06em] ">
         {labData.labName}
       </p>
-      <p className="font-josefin-sans font-normal text-[20px] text-white leading-[20px] tracking-[0.06em] ">
-        Lab Code : {labData.labCode}
-      </p>
+
+      <div className="px-4 bg-blue-300 rounded-[30px] cursor-pointer">EDIT</div>
+      <div className="px-4 bg-blue-300 rounded-[30px] cursor-pointer" onClick={deleteLab}>
+        DELETE
+      </div>
     </div>
   );
 };
--- src/Components/Pages/ViewLabs.jsx
+++ src/Components/Pages/ViewLabs.jsx
@@ -1,34 +1,100 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import checklist from "../../styles/images/checklist.png";
+import axios from "axios";
 
 import LabCard from "../LabCard";
 
 export const ViewLabs = () => {
+  const [error, setError] = useState(null);
+  const [newLab, setNewLab] = useState({
+    labName: "",
+    labCode: "",
+    imageURL: "",
+  });
+
   const location = useLocation();
-  const labs = location.state?.labs || [];
-  console.log(labs);
+  const [labs, setLabs] = useState(Array.isArray(location.state?.labs) ? location.state.labs : []);
+
+  const sendLab = async () => {
+    const labData = {
+      labName: newLab.labName,
+      labCode: newLab.labCode,
+      imageURL: newLab.imageURL.trim() ? newLab.imageURL : null,
+    };
+
+    if (!labData.labName || !labData.labCode) {
+      setError("Please fill in all the fields.");
+      console.log(error);
+      return;
+    }
+
+    try {
+      console.log(labData);
+      const response = await axios.post(
+        "http://ims-api-fbf3hheffacqe5ak.westus2-01.azurewebsites.net/api/admin/labs",
+        labData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      console.log("i am here");
+      console.log("New Lab Added:", response.data);
+      setLabs((prevLabs) => [...prevLabs, response.data]);
+      setNewLab({
+        labName: "",
+        labCode: "",
+        imageURL: "",
+      });
+    } catch (errror) {
+      console.error("Error when fetching res", error);
+      setError("Failed to load reservations");
+    }
+  };
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setNewLab((prev) => ({ ...prev, [name]: value }));
+  };
   return (
-    <div className="h-[600px] w-full bg-[#202652]  flex relative ">
-      <div className="h-full w-full flex justify-center items-center ">
-        <div className="h-full w-[1000px] grid grid-cols-2 gap-3">
-          <>
-            {/* <div className="flex justify-center items-center ">
-              <Link to="/view-labs">
-                <Card imgsrc={checklist} altname="admin profile" Children="LAB 01" />
-              </Link>
-            </div> */}
-            {labs.map((lab, index) => {
-              return (
-                <div key={index} className="flex justify-center items-center ">
-                  <Link to="/view-labs" className="flex justify-center items-center ">
-                    <LabCard imgsrc={checklist} altname="staff profile" labData={lab} />
-                  </Link>
-                </div>
-              );
-            })}
-          </>
+    <div className="h-full w-full bg-[#202652]  flex relative flex-col items-center justify-center p-10">
+      <div>
+        <div className="mt-10 p-2 px-4 bg-blue-300 rounded-[30px] cursor-pointer" onClick={sendLab}>
+          ADD LAB +
         </div>
+        <input type="text" name="labName" value={newLab.labName} onChange={handleInput} placeholder="Enter Lab Name" />
+        <input type="text" name="labCode" value={newLab.labCode} onChange={handleInput} placeholder="Enter Lab Code" />
+        <input
+          type="text"
+          name="imageURL"
+          value={newLab.imageURL}
+          onChange={handleInput}
+          placeholder="Enter Lab Image URL"
+        />
+
+        <div className="mt-10 p-2 px-4 bg-blue-300 rounded-[30px] cursor-pointer" onClick={sendLab}>
+          +
+        </div>
+      </div>
+
+      <div className="h-full w-[1000px]  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-items-center">
+        {labs.map((lab, index) => {
+          return (
+            <div key={index} className="flex justify-center items-center ">
+              <div className="flex justify-center items-center ">
+                <LabCard
+                  imgsrc={checklist}
+                  altname="staff profile"
+                  labData={lab}
+                  onLabDelete={(labId) => setLabs((prevLabs) => prevLabs.filter((delLab) => delLab.labId !== labId))}
+                />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
