0ec598c925df16c46dce4307a1c892fb5ff3e34b
Sharada Shehan <sharadashehan6@gmail.com>
2024-10-18 10:32:41 +0530

    auth redirect path changed

--- src/Components/Pages/SignIn.jsx
+++ src/Components/Pages/SignIn.jsx
@@ -47,7 +47,7 @@ function SignIn() {
     handleLoginRedirect();
   });
 
-  const redirectUri = "https://ims-api.azure-api.net/callback";
+  const redirectUri = "https://ims-api.azure-api.net";
   const loginUrl = `${AUTH_SERVER_URL}/login?redirectUri=${encodeURIComponent(redirectUri)}&clientId=${AUTH_CLIENT_ID}`;
 
   return (
